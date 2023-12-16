f499e8851d97d0e92df191c33b2ca4cc4c34fa8e
DAS <salkin_da@mail.ru>
2023-12-16 10:34:22 +0300

    Task_1

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿//ДЗ
+// Задача 1: 
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int[,] array = new int [3,4];
+
+void CreateArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i,j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void ReturnElementArray()
+{
+    System.Console.Write("Введите индекс строки элемента: ");
+    int iFind = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("Введите индекс столбца элемента: ");
+    int jFind = Convert.ToInt32(Console.ReadLine());
+    bool isFind = false;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == iFind && j == jFind)
+            {
+                isFind = true;
+                System.Console.WriteLine("Значение этого элемента " +array[i,j]);
+                break;
+            }
+        }
+    }
+    if (isFind == false)
+    {
+        System.Console.WriteLine("Такого элемента нет");
+    }
+    
+}
+
+CreateArray();
+PrintArray();
+ReturnElementArray();
